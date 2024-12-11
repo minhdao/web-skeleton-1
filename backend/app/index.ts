c6638e1b3d0b98d9ae7272b122d1b3c33a633a13
Minh Dao <minhdao6@gmail.com>
2024-12-11 05:51:16 +0000

    Create sign-up route

--- backend/app/index.ts
+++ backend/app/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import { handleSignUpUser } from './auth/sign-up/sign-up.handler';
 
 const app = express();
 const port = 8080;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello, this is backend');
 });
+
+app.post('/sign-up', handleSignUpUser);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
