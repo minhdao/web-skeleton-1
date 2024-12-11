c6f0ea6024e977fd1b93f1a29f6b71cfe860784e
Minh Dao <minhdao6@gmail.com>
2024-12-11 08:05:11 +0000

    Set SERVER_URL as const

--- frontend/src/api/server.ts
+++ frontend/src/api/server.ts
@@ -1,6 +1,6 @@
 const SERVER_HOST = 'http://localhost';
 const SERVER_PORT = '8080';
-const SERVER_URL = `${SERVER_HOST}:${SERVER_PORT}`;
+const SERVER_URL = `${SERVER_HOST}:${SERVER_PORT}` as const;
 
 const BACKEND_SERVER = {
   url: {
