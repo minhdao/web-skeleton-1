6624e8fb23e1b765ed17b63ca5d97870b72fd5af
Minh Dao <minhdao6@gmail.com>
2024-12-16 08:09:43 +0000

    Use .env variable to dynamically switch frontend base URL

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ''); // Third argument '' ensures all variables are loaded
+
+  return {
+    base: env.VITE_BASE_URL,
+    build: {
+      outDir: 'dist',
+    },
+    plugins: [react()],
+  };
+});
